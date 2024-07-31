3457ecefcdf1383826a6672030fcc5298d1b3d2d
Jefry Christiawan <jefrychristiawan2005@gmail.com>
2024-07-31 22:48:14 +0700

    Adding Backend for message

---
+++ backend/middleware/protectRoute.js
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken'
+
+const protectRoute = (req, res, next) => {
+    try{
+
+    } catch (error) {
+        console.log()
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js"
 
 const app = express()
@@ -12,6 +13,7 @@ dotenv.config()
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
 
 // app.get("/", (req, res) => {
 //     res.send("Hello World!!")
